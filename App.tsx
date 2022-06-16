6641a07160a212d0f055cf129ff81aeba9806d4f
Caio18-cosenza <caiiocosenza2020@gmail.com>
2022-06-16 00:54:44 -0300

    aprendendo #1

--- App.tsx
+++ App.tsx
@@ -2,19 +2,13 @@ import 'react-native-gesture-handler';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import { StatusBar } from 'expo-status-bar';
-import { Home } from './src/screens/Home';
+import Home from './src/screens/Home';
 
-function App() {
+export default function App() {
   return (
     <>
-      <StatusBar
-        style="light"
-        translucent
-        backgroundColor="transparent"
-      />
+      <StatusBar style='light' translucent backgroundColor='transparent' />
       <Home />
     </>
   );
 }
-
-export default gestureHandlerRootHOC(App);
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from 'react';
-import { FlatList } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { FlatList, Alert } from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import { Menu, MenuTypeProps } from '../../components/Menu';
 import { Skill } from '../../components/Skill';
@@ -8,52 +9,96 @@ import { Button } from '../../components/Button';
 
 import { Container, Title, Input, Form, FormTitle } from './styles';
 
-export function Home() {
-  const [type, setType] = useState<MenuTypeProps>("soft");
+import { database } from '../../config/database';
+import { Q } from '@nozbe/watermelondb';
+import { SkillModel } from '../../config/database/model/skillModel';
+
+function Home() {
+  const [type, setType] = useState<MenuTypeProps>('soft');
   const [name, setName] = useState('');
+  const [skills, setSkills] = useState<SkillModel[]>([]);
+  const [skill, setSkill] = useState<SkillModel>({} as SkillModel);
 
   const bottomSheetRef = useRef<BottomSheet>(null);
+
+  async function handleSave() {
+    if (skill.id) {
+      await database.write(async () => {
+        await skill.update((data) => {
+          (data.name = name), (data.type = type);
+        });
+        Alert.alert('Updated!');
+        setSkill({} as SkillModel);
+      });
+    } else {
+      await database.write(async () => {
+        await database.get<SkillModel>('skills').create((data) => {
+          (data.name = name), (data.type = type);
+        });
+      });
+      Alert.alert('Created!');
+    }
+
+    bottomSheetRef.current?.collapse();
+    fetchData();
+  }
+
+  async function handleRemove(item: SkillModel) {
+    await database.write(async () => {
+      await item.destroyPermanently();
+    });
+    fetchData();
+    Alert.alert('Deleted!');
+  }
+
+  async function handleEdit(item: SkillModel) {
+    setSkill(item);
+    setName(item.name);
+    bottomSheetRef.current?.expand();
+  }
+
+  async function fetchData() {
+    const skillCollection = database.get<SkillModel>('skills');
+    const response = await skillCollection.query(Q.where('type', type)).fetch();
+    setSkills(response);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [type]);
 
   return (
     <Container>
       <Title>About me</Title>
-      <Menu
-        type={type}
-        setType={setType}
-      />
+      <Menu type={type} setType={setType} />
 
       <FlatList
-        data={[]}
-        keyExtractor={item => item}
+        data={skills}
+        keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <Skill
             data={item}
-            onEdit={() => { }}
-            onRemove={() => { }}
+            onEdit={() => handleEdit(item)}
+            onRemove={() => handleRemove(item)}
           />
         )}
       />
 
-      <BottomSheet
-        ref={bottomSheetRef}
-        index={0}
-        snapPoints={['1%', '35%']}
-      >
+      <BottomSheet ref={bottomSheetRef} index={0} snapPoints={['5%', '35%']}>
         <Form>
-          <FormTitle>New</FormTitle>
+          <FormTitle> {skill.id ? 'Edit' : 'New'} </FormTitle>
 
           <Input
-            placeholder="New skill..."
+            placeholder='New skill...'
             onChangeText={setName}
             value={name}
           />
 
-          <Button
-            title="Save"
-            onPress={() => { }}
-          />
+          <Button title='Save' onPress={handleSave} />
         </Form>
       </BottomSheet>
     </Container>
   );
 }
+
+export default gestureHandlerRootHOC(Home);
